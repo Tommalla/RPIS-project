4f7e9ec5211f867ce7ab4f839717210c650b939a
Tomasz Zakrzewski <tz336079@students.mimuw.edu.pl>
2014-01-15 11:46:16 +0100

    Added initial version of the script

--- script.R
+++ script.R
@@ -1,3 +1,27 @@
 # Tomasz Zakrzewski, tz336079, RPIS 2013/2014
-x = runif(5, 0, 10)
-x
+estMean = function(x) {
+  return (2 * mean(x))
+}
+
+estMax = function(x) {
+  n = length(x)
+  return (max(x) * (n + 1) / n)
+}
+
+# place values here, since R sucks...
+k = 60
+a = 1000
+numTests = 1000
+
+meanRes = seq(numTests)
+maxRes = seq(numTests)
+
+for (i in seq(numTests)) {
+  x = runif(k, 0, a)
+  meanRes[i] = estMean(x)
+  maxRes[i] = estMax(x)
+}
+
+plot(meanRes, type="l", col="red", ylim=c(0, 2 * a), xlim=c(0, numTests))
+par(new=T)
+plot(maxRes, type="l", col="blue", ylim=c(0, 2 * a), xlim=c(0, numTests))
